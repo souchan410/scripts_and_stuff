477096387e77c68b71ba58af292ff993241de4a3
souchan410 <113465510+souchan410@users.noreply.github.com>
2022-12-23 21:48:24 +0400

    Added user input in nginx-log script

--- script_nginx-log.sh
+++ script_nginx-log.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-HOSTADDR=                           #print your remote host here
-SSHUSER=                            #print here your ssh username
-privatekey=                         #path to your privatekey
-LOCALDIR=                           #path to local directory
+#Setting up variables with user input
+read -p 'Your remote host adress: '     HOSTADDR                           #print your remote host here
+read -p 'Your remote username: '        SSHUSER                            #print here your ssh username
+read -p 'Your private shh-key file: '   privatekey=                         #path to your privatekey
 
 ssh $SSHUSER@$HOSTADDR -i $privatekey 'mkdir /usr/local/out_files'
-#extracting uniq IP's for last two hours from containerized NGINX access.log on remote host and write it to a file, then awk extracts unique IP's and write them to another file
-ssh $SSHUSER@$HOSTADDR -i $privatekey 'docker logs --since 2h $(docker ps | grep nginx | awk '\''{print $1}'\'') > /usr/local/out_files/logs-nginx.txt'
-ssh $SSHUSER@$HOSTADDR -i $privatekey 'awk '\''{print $1}'\'' /usr/local/out_files/logs-nginx.txt | sort | uniq -c > /usr/local/out_files/nginx-awk_log_$(date +%d_%b_%H_%M_%S).txt'
-
+#extracting uniq IP's for last two hours from containerized NGINX access.log on remote host and write it to a file, the>ssh $SSHUSER@$HOSTADDR -i $privatekey 'docker logs --since 2h $(docker ps | grep nginx | awk '\''{print $1}'\'') > /usr>ssh $SSHUSER@$HOSTADDR -i $privatekey 'awk '\''{print $1}'\'' /usr/local/out_files/logs-nginx.txt | sort | uniq -c > /u>
 #adding filename variable that we will need further
 filename=$(ssh $SSHUSER@$HOSTADDR -i $privatekey 'cd /usr/local/out_files/; ls -t | head -1')
+
+#setting up local dir path variable
+read -p 'Chose local directory to store log file: ' LOCALDIR
 
 #copy file from remote host using scp utility
 scp -i $privatekey $SSHUSER@$HOSTADDR:/usr/local/out_files/$filename $LOCALDIR/$filename
@@ -20,7 +20,8 @@ scp -i $privatekey $SSHUSER@$HOSTADDR:/usr/local/out_files/$filename $LOCALDIR/$
 #ssh $SSHUSER@$HOSTADDR -i $privatekey 'rm -rf /usr/local/out_files/$filename'
 ssh $SSHUSER@$HOSTADDR -i $privatekey 'rm /usr/local/out_files/logs-nginx.txt'
 
-#rm $LOCALDIR$filename
-
-
+#print results to terminal
 cat /home/linux/$filename
+
+#remove log file
+#rm $LOCALDIR$filename
